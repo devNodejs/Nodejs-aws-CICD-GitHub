e19a04b3ad7a4fe9ab7ea146f08b709bf15665dd
santosh singh <singhsantosh.edu.in@gmail.com>
2020-11-24 11:41:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+import 'babel-polyfill';
+import express from 'express';
+const app = express ();
+
+const port = 9494;
+
+app.get ('/', (req, res) => {
+  res.send ('Welcome to the Home page..........');
+});
+
+app.listen (port, () => {
+  console.log (
+    `CI-CD-GitHub-aws-server Application listening on port ${port} !`
+  );
+});
